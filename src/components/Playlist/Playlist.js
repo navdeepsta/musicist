730224b3c63b83281411eefc59eff59773bee046
navdeep <navdeepst.1987@gmail.com>
2023-06-29 14:26:32 +1000

    Add form input for playlist name

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import Button from '../Button/Button'
 import List from '../List/List'
+import styles from './Playlist.module.css'
+
 export default function Playlist(props) {
+  const {onChangePlaylistName, playlistName} = props;
+  
+  const handleChange = ({target})=>{
+    onChangePlaylistName(target.value);
+  }
+
   return (
-    <div>Playlist
+    <div>
+      <input 
+        className = {styles.border}
+        placeholder="Playlist name"
+        onChange = {handleChange}
+        value = {playlistName}
+       
+      /> 
       <List tracks={props.tracks}/> <Button name="Save To Spotify"/>
     </div>
   )
--- src/containers/SearchResults.js
+++ src/containers/SearchResults.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Tracklist from '../components/Tracklist/Tracklist'
 import Playlist from '../components/Playlist/Playlist'
 
 export default function SearchResults(props) {
+
+  const [playlistName, setPlaylistName] = useState('');
+
+  const handlePlaylistName = (name)=>{
+    setPlaylistName(name);
+  }
+
   return (
     <div>
       <Tracklist tracks={props.tracks}/>
-      <Playlist tracks={props.tracks}/>
+      <Playlist 
+        playlistName={playlistName} 
+        onChangePlaylistName={handlePlaylistName} 
+      />
     </div>
   )
 }
